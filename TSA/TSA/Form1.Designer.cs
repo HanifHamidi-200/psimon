a86e73e87f4aafc3eae6780a23a7705efdccbd9e
Hanif Hamidi <Hanif@Melayu.org.uk>
2019-02-20 09:37:41 +0000

    TSA update

--- TSA/TSA/Form1.Designer.cs
+++ TSA/TSA/Form1.Designer.cs
@@ -31,6 +31,9 @@
             this.btnOpen1 = new System.Windows.Forms.Button();
             this.btnOpen2 = new System.Windows.Forms.Button();
             this.btnOpen3 = new System.Windows.Forms.Button();
+            this.btnOpen6 = new System.Windows.Forms.Button();
+            this.btnOpen5 = new System.Windows.Forms.Button();
+            this.btnOpen4 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btnOpen1
@@ -51,7 +54,7 @@
             this.btnOpen2.Name = "btnOpen2";
             this.btnOpen2.Size = new System.Drawing.Size(249, 44);
             this.btnOpen2.TabIndex = 1;
-            this.btnOpen2.Text = "TSA";
+            this.btnOpen2.Text = "TSA_LSA";
             this.btnOpen2.UseVisualStyleBackColor = false;
             this.btnOpen2.Click += new System.EventHandler(this.btnOpen2_Click);
             // 
@@ -66,11 +69,44 @@
             this.btnOpen3.UseVisualStyleBackColor = false;
             this.btnOpen3.Click += new System.EventHandler(this.btnOpen3_Click);
             // 
+            // btnOpen6
+            // 
+            this.btnOpen6.BackColor = System.Drawing.Color.Yellow;
+            this.btnOpen6.Location = new System.Drawing.Point(28, 300);
+            this.btnOpen6.Name = "btnOpen6";
+            this.btnOpen6.Size = new System.Drawing.Size(249, 44);
+            this.btnOpen6.TabIndex = 5;
+            this.btnOpen6.Text = "ManoevringBit";
+            this.btnOpen6.UseVisualStyleBackColor = false;
+            // 
+            // btnOpen5
+            // 
+            this.btnOpen5.BackColor = System.Drawing.Color.Yellow;
+            this.btnOpen5.Location = new System.Drawing.Point(28, 250);
+            this.btnOpen5.Name = "btnOpen5";
+            this.btnOpen5.Size = new System.Drawing.Size(249, 44);
+            this.btnOpen5.TabIndex = 4;
+            this.btnOpen5.Text = "SteelmakingPlants_SERPO";
+            this.btnOpen5.UseVisualStyleBackColor = false;
+            // 
+            // btnOpen4
+            // 
+            this.btnOpen4.BackColor = System.Drawing.Color.Yellow;
+            this.btnOpen4.Location = new System.Drawing.Point(28, 200);
+            this.btnOpen4.Name = "btnOpen4";
+            this.btnOpen4.Size = new System.Drawing.Size(249, 44);
+            this.btnOpen4.TabIndex = 3;
+            this.btnOpen4.Text = "PlanesJetsAircraftcarriers";
+            this.btnOpen4.UseVisualStyleBackColor = false;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(297, 269);
+            this.ClientSize = new System.Drawing.Size(297, 376);
+            this.Controls.Add(this.btnOpen6);
+            this.Controls.Add(this.btnOpen5);
+            this.Controls.Add(this.btnOpen4);
             this.Controls.Add(this.btnOpen3);
             this.Controls.Add(this.btnOpen2);
             this.Controls.Add(this.btnOpen1);
@@ -85,6 +121,9 @@
         private System.Windows.Forms.Button btnOpen1;
         private System.Windows.Forms.Button btnOpen2;
         private System.Windows.Forms.Button btnOpen3;
+        private System.Windows.Forms.Button btnOpen6;
+        private System.Windows.Forms.Button btnOpen5;
+        private System.Windows.Forms.Button btnOpen4;
     }
 }
 
